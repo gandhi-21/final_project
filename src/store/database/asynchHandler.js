d8e4e634314d223ec2be8382c29e9df272cce568
Gaurav Gandhi <Gaurav.Gandhi@stonybrook.edu>
2019-12-15 23:02:24 -0500

    improved register screen

--- src/store/database/asynchHandler.js
+++ src/store/database/asynchHandler.js
@@ -27,6 +27,8 @@ export const registerHandler = (newUser, firebase) => (dispatch, getState, { get
         firstName: newUser.firstName,
         lastName: newUser.lastName,
         initials: `${newUser.firstName[0]}${newUser.lastName[0]}`,
+        email: newUser.email,
+        isadmin: false
     })).then(() => {
         dispatch(actionCreators.registerSuccess);
     }).catch((err) => {
